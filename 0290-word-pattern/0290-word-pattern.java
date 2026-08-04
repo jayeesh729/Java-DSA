95faf77f81c5ba54a67ef476c2fa21b7ca82ad21
Jayeessh <jayeessh230605@gmail.com>
2026-08-04 23:12:20 +0530

    Time: 1 ms (83.31%), Space: 42.6 MB (74.52%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String words[] = s.split(" ");
+        if(words.length != pattern.length()){
+            return false;
+        }
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+    
+        for(int i=0; i<pattern.length() ; i++){
+            String word = words[i];
+            char ch = pattern.charAt(i);
+            if( !charToWord.containsKey(ch) ){
+                charToWord.put(ch, word);
+            }
+            if( !wordToChar.containsKey(word) ){
+                wordToChar.put(word, ch);
+            }
+            if(!charToWord.get(ch).equals(word) || !wordToChar.get(word).equals(ch) ){
+                return false;
+            }
+        }
+        return true;
+    }
+}
